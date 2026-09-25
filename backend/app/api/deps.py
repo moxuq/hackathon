d8544de4bc02d213dec1686e559b8e4a94ebb057
Mox <gromovs252@gmail.com>
2026-09-26 01:27:57 +0300

    Created deps.py with dependecies

---
+++ backend/app/api/deps.py
@@ -0,0 +1,17 @@
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from ..core.redis_client import get_redis_client
+from ..core.security import get_user_by_token
+from ..models.base import get_db
+from ..models.user import User
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/api/v1/auth/login')
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)) -> User:
+    user = await get_user_by_token(token, db)
+    return user
+
+async def get_redis():
+    return get_redis_client()
